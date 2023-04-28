9c44c1a4fe945e43424f7ce26efa071a96f02e9a
datsaker20 <81208546+datsaker20@users.noreply.github.com>
2023-04-28 16:01:37 +0700

    Update

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from 'cors'
 import path from "path"
+import { fileURLToPath } from "url";
 // configure env
 dotenv.config();
 
@@ -16,6 +17,10 @@ const app = express();
 
 // database configuration
 connectDB();
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 // Phần trung gian
 app.use(cors());
 app.use(express.json());
